5075f45a889085155a6cc969e67a1e69ec082af7
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-02-17 17:35:17 -0500

    created seeds

---
+++ data/seeds/04-step_ingredients.js
@@ -0,0 +1,20 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('steps_ingredients').del()
+    .then(function () {
+      return knex('steps_ingredients').insert([
+        {recipe_id: 1, step_id: 1, ingredient_id: 1, quantity: 5},
+        {recipe_id: 1, step_id: 2, ingredient_id: 2, quantity: 3},
+        {recipe_id: 1, step_id: 2, ingredient_id: 3, quantity: 0.13},
+        {recipe_id: 1, step_id: 2, ingredient_id: 4, quantity: 0.13},
+        {recipe_id: 1, step_id: 3, ingredient_id: 5, quantity: 2},
+        {recipe_id: 1, step_id: 3, ingredient_id: 6, quantity: 3},
+        {recipe_id: 1, step_id: 3, ingredient_id: 7, quantity: 1},
+        {recipe_id: 2, step_id: 2, ingredient_id: 8, quantity: 2},
+        {recipe_id: 2, step_id: 2, ingredient_id: 9, quantity: 2},
+        {recipe_id: 2, step_id: 1, ingredient_id: 10, quantity: 2},
+        {recipe_id: 1, step_id: 2, ingredient_id: 12, quantity: 5},
+        {recipe_id: 3, step_id: 7, ingredient_id: 11, quantity: 1},
+      ]);
+    });
+};
